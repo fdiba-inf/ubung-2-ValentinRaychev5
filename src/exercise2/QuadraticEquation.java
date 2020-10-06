df4b11487e734b94bee58db0a1ddc4625745738d
ValentinRaychev <valentin.raychev@fdiba.tu-sofia.bg>
2020-10-06 21:15:18 +0000

    exercise2

--- src/exercise2/QuadraticEquation.java
+++ src/exercise2/QuadraticEquation.java
@@ -21,8 +21,7 @@ public class QuadraticEquation {
             x1 = ((-b) + Math.sqrt(D)) / (2.0*a); 
             x2 = ((-b) - Math.sqrt(D)) / (2.0*a);
 
-            System.out.println("x1: " + x1);
-            System.out.println("x2: " + x2);
+            System.out.println(x1 + ", " + x2);
           }
           else  {
             System.out.println("Imaginary values");
